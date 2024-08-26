21722729919fc12ae756e8a42bbfea40c03250b0
Ignacio Loyola <ignacio.loyola@biomedica.udec.cl>
2024-08-26 19:35:31 -0400

    handle exceptions

--- main.py
+++ main.py
@@ -44,6 +44,25 @@ class InstaUser:
     def close_browser(self):
         self.driver.close()
 
+    def handle_exceptions(self):
+        try:
+            # Click "Not now" and ignore Save-login info prompt
+            save_login_prompt = WebDriverWait(self.driver, MIN_TIME_LOAD).until(
+                EC.presence_of_element_located((By.XPATH, xpath["save_login_not_now_button"]))
+            )
+            save_login_prompt.click()
+        except:
+            pass
+
+        try:
+            # Click "not now" on notifications prompt
+            notifications_prompt = WebDriverWait(self.driver, MIN_TIME_LOAD).until(
+                EC.presence_of_element_located((By.XPATH, xpath["notification_not_now_button"]))
+            )
+            notifications_prompt.click()
+        except:
+            pass
+        
     def login(self):
         self.driver.get(urls["login"])
 
@@ -125,6 +144,7 @@ class InstaUser:
 
     def openchat(self):
         self.driver.get("https://www.instagram.com/direct/inbox/")
+        self.exceptions()
 
 bot = InstaUser()
 bot.login()
